ce9b995b7a4c821935e9aafde01dbd42a991fcb2
dinhthitu <168218299+dinhthitu@users.noreply.github.com>
2025-06-08 21:24:27 +0700

    Update user.js

--- backend/backend/routes/user.js
+++ backend/backend/routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { deleteUser, getUser, updateUser, register, checkOtp, login } = require('../controllers/userController');
+const { register, checkOtp, login, getUser } = require('../controllers/userController');
 const { verifyToken, verifyUser } = require('../utils/verifyToken');
 const { registerUserValidate, loginUserValidate } = require('../validate/user');
 const multer = require('multer');
 const { uploadImageToCloudinary } = require('../middlewares/uploadImageToCloudinary');
-const {User} = require('../models'); 
+// const {User} = require('../models'); 
 
 const upload = multer({ storage: multer.memoryStorage() });
 
@@ -15,22 +15,13 @@ router.post('/login', loginUserValidate, login);
 router.get('/getuser', verifyToken, getUser);
 
 
-
 router.post('/logout', (req, res) => {
   res.clearCookie('token', { httpOnly: true, secure: true, sameSite: 'none' })
     .status(200)
     .json({ success: true, message: 'Logged out successfully' });
 });
 
-router.get('/check/:id', verifyUser, (req, res) => {
-  res.send('Hello user, you\'re logged in and can delete your account');
-});
 
-
-
-
-router.put('/:id', verifyUser, updateUser);
-router.delete('/:id', verifyUser, deleteUser);
 router.get('/:id', verifyUser, getUser);
 
 
