6c5c509eb6806e3c01c3776527e0691f73b2b3ea
dinhthitu <168218299+dinhthitu@users.noreply.github.com>
2025-06-17 16:39:59 +0700

    Update wishlist.js

--- backend/backend/routes/wishlist.js
+++ backend/backend/routes/wishlist.js
@@ -6,26 +6,21 @@ const {
   getWishlist,
   addToWishlist,
   removeFromWishlist,
-  clearWishlist,
   checkInWishlist,
   getUserWishlist,
-  addRecipeToUserWishlist,
   deleteRecipeFromWishlist
 } = require("../controllers/wishlistController");
 
-// Apply checkToken for all routes to ensure authentication
 router.use(checkToken);
 
 // Routes for all authenticated users
 router.get("/wishlist", getWishlist);
 router.post("/wishlist/:recipeId", addToWishlist);
 router.delete("/wishlist/:recipeId", removeFromWishlist);
-router.delete("/wishlist", clearWishlist);
 router.get("/wishlist/check/:recipeId", checkInWishlist);
 
 // Routes for admins only
 router.get("/admin/:userId", isAdmin, getUserWishlist);
-router.post("/admin/:userId/:recipeId", isAdmin, addRecipeToUserWishlist);
 router.delete("/admin/:userId/:recipeId", isAdmin, deleteRecipeFromWishlist);
 
 module.exports = router;
