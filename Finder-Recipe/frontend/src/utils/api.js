42221d0ba9d40494b009d3a7fb09979f0ac928f9
dinhthitu <168218299+dinhthitu@users.noreply.github.com>
2025-06-17 16:47:43 +0700

    Update api.js

--- Finder-Recipe/frontend/src/utils/api.js
+++ Finder-Recipe/frontend/src/utils/api.js
@@ -6,34 +6,9 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true, // Thêm để gửi cookie nếu cần
+  withCredentials: true, 
 });
 
-// Interceptor để xử lý request trước khi gửi
-// api.interceptors.request.use(
-//   (config) => {
-//     // let token = localStorage.getItem('token');
-//     // if (!token) {
-//     //   token = document.cookie
-//     //     .split('; ')
-//     //     .find(row => row.startsWith('token='))
-//     //     ?.split('=')[1];
-//     //   console.log('Token from cookie:', token);
-//     //   if (token) {
-//     //     localStorage.setItem('token', token);
-//     //   }
-//     // }
-//     // console.log('Token being sent in header:', token);
-//     // if (token) {
-//     //   config.headers.Authorization = `Bearer ${token}`;
-//     // }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-// Update your api.js file
 api.interceptors.request.use(
   (config) => {
     let token = localStorage.getItem("token");
@@ -57,7 +32,6 @@ api.interceptors.request.use(
   }
 );
 
-// Interceptor để xử lý response
 api.interceptors.response.use(
   (response) => response.data,
   async (error) => {
@@ -65,7 +39,7 @@ api.interceptors.response.use(
       console.error('Unauthorized - Redirecting to login');
       localStorage.removeItem('token');
       localStorage.removeItem('user');
-      window.location.href = '/login'; // Chuyển hướng đến trang đăng nhập
+      window.location.href = '/login'; 
     }
     if (error.response) {
       console.error('API Error:', error.response.data);
@@ -80,17 +54,6 @@ export const getUserApi = async () => {
     return response;
   } catch (error) {
     return { success: false, message: error.response?.data?.message || "Error fetching user" };
-  }
-};
-export const deleteUserRecipeApi = async (userId, recipeId) => {
-  try {
-    const response = await api.delete(`/wishlist/admin/${userId}/${recipeId}`);
-    return response;
-  } catch (error) {
-    return {
-      success: false,
-      message: error.response?.data?.message
-    };
   }
 };
 
