096f18b1ed7445524ed8bc45c82fe55969de25a8
dinhthitu <168218299+dinhthitu@users.noreply.github.com>
2025-06-17 14:23:21 +0700

    Update api.js

--- Finder-Recipe/frontend/Axios/client/api.js
+++ Finder-Recipe/frontend/Axios/client/api.js
@@ -1,3 +1,135 @@
+// import axios from "../axios.custom";
+
+// const registerUser = async (data) => {
+//   try {
+//     const URL_LOGIN = '/users/register';
+//     const response = await axios.post(URL_LOGIN, data, {
+//       withCredentials: true,
+//     });
+//     return response;
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error?.response?.data?.message || "Error in axios",
+//     };
+//   }
+// };
+
+// const checkTokenOtp = async (data) => {
+//   try {
+//     const URL_LOGIN = '/users/checkOtp';
+//     const response = await axios.post(URL_LOGIN, data, { withCredentials: true });
+//     return response;
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error?.response?.data?.message || "Error in axios",
+//     };
+//   }
+// };
+
+// const getUserApi = async () => {
+//   try {
+//     const URL_LOGIN = '/users/getuser';
+//     const response = await axios.get(URL_LOGIN, { withCredentials: true });
+//     return response;
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error?.response?.data?.message || "Error in axios",
+//     };
+//   }
+// };
+
+// const loginApi = async (data) => {
+//   try {
+//     const URL_LOGIN = '/users/login';
+//     const response = await axios.post(URL_LOGIN, data, { withCredentials: true });
+//     return response;
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error?.response?.data?.message || "Error in axios",
+//     };
+//   }
+// };
+
+// const getAllUsersApi = async () => {
+//   try {
+//     const URL = '/admin/users';
+//     const response = await axios.get(URL, { withCredentials: true });
+//     return response;
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error?.response?.data?.message || "Error in axios",
+//     };
+//   }
+// };
+
+// const createUserApi = async (data) => {
+//   try {
+//     const URL = '/users/create';
+//     const response = await axios.post(URL, data, { withCredentials: true });
+//     return response;
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error?.response?.data?.message || "Error in axios",
+//     };
+//   }
+// };
+
+// const updateUserApi = async (id, data) => {
+//   try {
+//     const URL = `/admin/users/${id}`;
+//     const response = await axios.put(URL, data, { withCredentials: true });
+//     return response;
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error?.response?.data?.message || "Error in axios",
+//     };
+//   }
+// };
+
+// const deleteUserApi = async (id) => {
+//   try {
+//     const URL = `/admin/users/${id}`;
+//     const response = await axios.delete(URL, { withCredentials: true });
+//     return response;
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error?.response?.data?.message || "Error in axios",
+//     };
+//   }
+// };
+
+// const getUserRecipesApi = async (userId) => {
+//   try {
+//     const URL = `/admin/users/${userId}/recipes`;
+//     const response = await axios.get(URL, { withCredentials: true });
+//     return response;
+//   } catch (error) {
+//     return {
+//       success: false,
+//       message: error?.response?.data?.message || "Error in axios",
+//     };
+//   }
+// };
+// export {
+//   registerUser,
+//   checkTokenOtp,
+//   getUserApi,
+//   loginApi,
+//   getAllUsersApi,
+//   createUserApi,
+//   updateUserApi,
+//   deleteUserApi,
+//   getUserRecipesApi,
+  
+// };
 import axios from "../axios.custom";
 
 const registerUser = async (data) => {
@@ -69,7 +201,7 @@ const getAllUsersApi = async () => {
 
 const createUserApi = async (data) => {
   try {
-    const URL = '/users/create';
+    const URL = '/admin/users/create';
     const response = await axios.post(URL, data, { withCredentials: true });
     return response;
   } catch (error) {
@@ -118,6 +250,18 @@ const getUserRecipesApi = async (userId) => {
     };
   }
 };
+const deleteUserRecipeApi = async (userId, recipeId) => {
+  try {
+    const URL = `/wishlist/admin/${userId}/${recipeId}`;
+    const response = await axios.delete(URL, { withCredentials: true });
+    return response;
+  } catch (error) {
+    return {
+      success: false,
+      message: error?.response?.data?.message || "Error in axios",
+    };
+  }
+};
 export {
   registerUser,
   checkTokenOtp,
@@ -128,5 +272,5 @@ export {
   updateUserApi,
   deleteUserApi,
   getUserRecipesApi,
-  
+  deleteUserRecipeApi
 };
